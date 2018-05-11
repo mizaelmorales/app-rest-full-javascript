f8bb571e225cbca7a3470b28b331190f7877df72
Mizael Morales <mizael754@gmail.com>
2018-05-10 21:42:27 -0500

    avance empaquetando con browsefify

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 //gulp-rename
 var rename = require('gulp-rename');
-
+//requerir babel para poder usar el ultimo ecmascript
+var babel = require('babelify');
+//requerimos el browsify como emsanmblador
+var browserify = require('browserify');
+//para que gulp reciba el bundle
+var source = require('vinyl-source-stream');
 gulp.task('styles', function(){
     gulp
         .src('index.scss')
@@ -19,4 +24,13 @@ gulp.task('assets', function (){
         .pipe(gulp.dest('public'));
 })
 
-gulp.task('default',['styles', 'assets'])
+gulp.task('scripts', function () {
+    browserify('./src/index.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+})
+
+gulp.task('default',['styles', 'assets','scripts']);
