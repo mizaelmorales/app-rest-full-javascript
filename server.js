5c1ad0cd78d0014f8d599397807f3a49f3b783bc
Mizael Morales <mizael754@gmail.com>
2018-05-22 13:13:15 -0500

    mod

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(express.static('public'));
 app.get('/', function (req,res){
     res.render('index');
 })
+app.get('/signup', function (req,res){
+    res.render('index');
+})
+app.get('/signin', function (req,res){
+    res.render('index');
+})
 //para iniciar el servidor
 app.listen(3000, function (err){
     if (err) return console.log('Hubo un error'), process.exit(1);
--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ var page = require ('page');
 
 var main = document.getElementById('main-container');
 
-page('/',)
+page('/', function(ctx,next){
+    main.innerHTML = 'home <a href="/signup">signup</a>'; //home
+})
+
+page('/signup', function(ctx,next){
+    main.innerHTML = 'Signup <a href="/">home</a>'//signup
+})
+
+page();
