a36656f5401523c6c053dca5d9c095102b9d9946
mizael <mizael754@gmail.com>
2018-06-19 10:32:21 -0500

    ULTIMO TEMPLATE

---
+++ src/signup/template.js
@@ -0,0 +1,39 @@
+var yo = require('yo-yo');
+module.export = yo`<div class="container">
+    <div class="row">
+       <div class="col s10 push-s1">
+           <div class="row">
+               <div class="col m5 hide-on-small-only">
+                   <img class="iphone" src="iphone.png">
+               </div>
+               <div class="col s12 m7">
+                    <div class="row">
+                        <div class="signup-form">
+                            <h1 class="platzigram" >Mizagram</h1>
+                            <form class="signup-box">
+                                <h2>Registrate para ver fotos de tus amigos estudiando en Mizagram</h2>
+                                    <div class="section">
+                                        <a class="btn btn-fb hide-on-small-only">Iniciar sesion con Facebook</a>
+                                        <a class="btn btn-fb hide-on-med-and-up">Iniciar sesion</a>
+                                    </div>
+                                    <div class="divider"></div>
+                                    <div class="section">
+                                        <input type="email" name="email" placeholder="Correo Electronico">
+                                        <input type="text" name="name" placeholder="Nombre Completo">
+                                        <input type="text" name="email" placeholder="Nombre de usuario">
+                                        <input type="password" name="password" placeholder="Contraseña">
+                                        <button class="btn waves-effect waves-light center"  type="submit">Registrate</button>
+                                    </div>
+                                
+                            </form>
+                        </div>
+                    
+                </div> 
+                <div class="row center">
+                        <div class="login-box">¿Tienes un cuenta?<a href="/signin">Entrar</a>
+                        </div>          
+                    </div>    
+            </div>
+       </div> 
+    </div>
+    </div>`;
